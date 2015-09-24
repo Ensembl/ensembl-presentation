bdfe8dd52c9e1e75caf2cd5eac2d006c23b95fb4
Matthieu Muffato <muffato@ebi.ac.uk>
2015-09-24 10:15:20 +0100

    Added the materials of the latest Compara workshop

---
+++ API/Compara/exercises/gene_tree2.pl
@@ -0,0 +1,33 @@
+use strict;
+use warnings;
+
+use Bio::EnsEMBL::Registry;
+
+## Load the registry automatically
+my $reg = "Bio::EnsEMBL::Registry";
+$reg->load_registry_from_url('mysql://anonymous@ensembldb.ensembl.org');
+
+## Get the compara gene member adaptor
+my $gene_member_adaptor = $reg->get_adaptor("Multi", "compara", "GeneMember");
+
+## Get the compara gene tree adaptor
+my $gene_tree_adaptor = $reg->get_adaptor("Multi", "compara", "GeneTree");
+
+## Get the compara member
+my $gene_member = $gene_member_adaptor->fetch_by_stable_id("ENSG00000238344");
+
+## Get the tree for this member
+my $tree = $gene_tree_adaptor->fetch_default_for_Member($gene_member);
+
+print "The tree contains the following genes:\n";
+foreach my $leaf_gene (@{$tree->get_all_Members()}) {
+## $tree->get_all_leaves() returns exactly the same list
+#foreach my $leaf_gene (@{$tree->get_all_leaves()}) {
+  print $leaf_gene->stable_id(), "\n";
+}
+
+## BioPerl alignment
+my $simple_align = $tree->get_SimpleAlign(-append_taxon_id => 1);
+my $alignIO = Bio::AlignIO->newFh(-format => "clustalw");
+print $alignIO $simple_align;
+

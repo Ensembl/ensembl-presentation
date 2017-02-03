6e8a89d443af39a16ef8e6f58aacde6fb0d82ad8
Myrto Kostadima <kostadim@ebi.ac.uk>
2017-02-03 13:06:27 +0000

    adding binding activiity exrcise and output

--- API/Regulation/display_script/binding_affinity1.pl
+++ API/Regulation/display_script/binding_affinity1.pl
@@ -9,70 +9,59 @@
 use strict;
 use warnings;
 use Bio::EnsEMBL::Registry;
-# use feature qw(say);
-# use Data::Dumper;
 
 # Load EnsEMBL Registry
 Bio::EnsEMBL::Registry->load_registry_from_db(
-    -host => 'ensembldb.ensembl.org',
-    -user => 'anonymous'
+               -host => 'ensembldb.ensembl.org',
+               -user => 'anonymous'
 );
 
-# Get the gene adaptor
-my $gene_adaptor  = $registry->get_adaptor( 'Human', 'Core', 'Gene' );
-
-# Retrieve all genes with name 'ESPN'
-my $gene_name = 'ESPN';
-my @genes     = @{ $gene_adaptor->fetch_all_by_external_name($gene_name) };
-
-# Get the splice adaptor
-my $slice_adaptor = $registry->get_adaptor( 'Human', 'Core', 'Slice' );
+my $rf_stable_id = 'ENSR00000000667';
 
 # Get the regulatory feature adaptor
-my $regulatory_feature_adaptor = $registry->get_adaptor( 'Human', 'Funcgen', 'Regulatoryfeature' );
+my $regulatory_feature_adaptor = Bio::EnsEMBL::Registry->get_adaptor('Human', 'Funcgen', 'RegulatoryFeature');
+# Retrieve the regulatory feature object using the stable identifier
+my $regulatory_feature = $regulatory_feature_adaptor->fetch_by_stable_id( $rf_stable_id );
+# Retrieve the regulatory feature slice
+my $rf_slice = $regulatory_feature->feature_Slice;
 
-# For each of the retrieved genes create a new slice instance
-# consisting of the region 1000bp upstream of the TSS of each 'ESPN' gene
-foreach my $gene ( @genes ) {
+# Get the motif adaptor
+my $motif_adaptor  = Bio::EnsEMBL::Registry->get_adaptor('Human', 'Funcgen', 'MotifFeature');
 
-  # Extract the information about the strand, the coordinate system
-  # and the chromosome on which the gene is located
-  my $strand          = $gene->strand;
-  my $coord_system    = $gene->slice->coord_system;
-  my $seq_region_name = $gene->seq_region_name;
+# Retrieve all the motifs on the Regulatory feature
+my @motifs = @{ $motif_adaptor->fetch_all_by_Slice( $rf_slice )};
 
-  # The start coordinate of the new slice is set to 0
-  my $slice_start = 0;
-  # The end coordinate of the new slice is set to 0
-  my $slice_end   = 0;
+# Loop through the motifs until you found the one 
+# for matrix 'PB0010.1' with sequence 'CCCGCCCCCACCCC'
+foreach my $motif ( @motifs ) {
 
-  # Check on which strand the gene is located and calculate
-  # the coordinates of the upstream region accordingly
-  if( $gene->strand eq 1 )
-  {
-    $slice_start = $gene->seq_region_start - 1000;
-    $slice_end   = $gene->seq_region_start;
-  } else
-  {
-    $slice_start = $gene->seq_region_start;
-    $slice_end   = $gene->seq_region_start + 1000;
-  }
+    if ( $motif->binding_matrix->name eq 'PB0010.1' && $motif->seq eq 'CCCGCCCCCACCCC') {
 
-  # Create a new slice instance for the promoter region
-  my $slice_gene = Bio::EnsEMBL::Slice->new(
-    -coord_system     => $coord_system,
-    -start            => $slice_start,
-    -end              => $slice_end,
-    -strand           => $strand,
-    -seq_region_name  => $seq_region_name,
-    -adaptor          => $slice_adaptor,
-    );
+        # Calculate the relative binding affinity of this motif 
+        my $slice             = $motif->feature_Slice;
+        my $sequence          = $motif->seq;
+        my $binding_matrix    = $motif->binding_matrix;
+        my $relative_affinity = $binding_matrix->relative_affinity($sequence);
 
-  my $regulatory_features = $regulatory_feature_adaptor->fetch_all_by_Slice( $slice_gene );
+        print "Relative affinity of the "
+              . $binding_matrix->name
+              . " motif with sequence\t"
+              . $sequence
+              . " : "
+              . $relative_affinity ."\n";
 
-  foreach my $regulatory_feature ( @$regulatory_features ) {
-    # Print some regulatory feature info
-    print "Regulatory Feature ID:  " . $regulatory_feature->stable_id . "\n";
-    print "Regulatory Feature Type:   " . $regulatory_feature->type . "\n";
-  }
+        # Calculate the relative binding affinity of the changed motif 
+        my $changed_sequence          = "CCCGGCCCCACCCC";
+        my $changed_relative_affinity = $binding_matrix->relative_affinity($changed_sequence);
+
+        print "Relative affinity of the "
+              . $binding_matrix->name
+              . " motif with sequence\t"
+              . $changed_sequence
+              . " : "
+              . $changed_relative_affinity ."\n";
+
+        print "                                                \t    ^\n";
+    }
 }
+  

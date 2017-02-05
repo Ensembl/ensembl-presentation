93ee9436a9dbc8d72b25b46c56840fa3b4e34cc9
Michael Nuhn <mnuhn@ebi.ac.uk>
2017-02-05 15:33:10 +0000

    Script to run all examples

---
+++ API/Regulation/run_all.bash
@@ -0,0 +1,14 @@
+perl display_script/find_crispr_sites_around_gene._pl
+
+perl display_script/microarrays1._pl
+perl display_script/microarrays2._pl
+perl display_script/microarrays3._pl
+
+perl display_script/binding_affinity._pl
+perl display_script/binding_affinity1._pl
+
+perl display_script/motif_feature1._pl
+
+perl display_script/regulatory_feature1._pl
+
+

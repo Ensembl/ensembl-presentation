39eb4a9a2a6aa2775fc5514136f739647fc6dd7a
Michael Nuhn <mnuhn@ebi.ac.uk>
2017-02-12 04:06:10 +0000

    b

---
+++ API/Regulation/run_all_perl.bash
@@ -0,0 +1,21 @@
+perl display_script/find_crispr_sites_around_gene._pl
+
+perl display_script/microarrays1._pl
+perl display_script/microarrays2._pl
+perl display_script/microarrays3._pl
+
+# perl display_script/binding_affinity._pl
+perl display_script/binding_affinity1._pl
+perl display_script/motif_feature1._pl
+
+perl display_script/regulatory_feature1._pl
+
+perl display_script/regulatory_features/epigenomes_in_regulatory_build._pl
+perl display_script/regulatory_features/fetch_regulatory_feature._pl
+perl display_script/regulatory_features/regulatory_activity._pl
+
+perl display_script/wiggletools/H3K27ac_epigenomes_in_regulation._pl
+perl display_script/wiggletools/H3K27ac_epigenomes_in_regulation_wiggletracks._pl
+
+
+
